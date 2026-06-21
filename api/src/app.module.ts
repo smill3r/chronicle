f247ab85780d17da9659de4cf6a797460fd9fc3f
Jose Smiller <smillerjess@gmail.com>
2026-06-21 10:34:34 +0200

    Add env var ref to client and api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,7 +4,7 @@ import { TimelinesModule } from './timelines/timelines.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/chronicle'),
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/chronicle'),
     TimelinesModule,
   ],
 })
